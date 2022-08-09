8ed5c6f4e77f0fecf41dd3021c1a3375d8f9749e
Everton Figueirôa <evertonbpg@gmail.com>
2022-08-09 02:36:20 -0300

    Projeto Jão em desenvolvimento

--- Jao.py
+++ Jao.py
@@ -8,6 +8,7 @@ import speech_recognition as sr
 import pygame
 #converso de texto para audio
 import pyttsx3
+from time import sleep
 
 
 def converVOZ(texto):
@@ -15,8 +16,10 @@ def converVOZ(texto):
 	engine.say(texto)
 	engine.runAndWait()
 
-
-converVOZ('BEM-VINDO EVERTON-SAMA, COMO POSSO AJUDAR')
+print('-=-'*20)
+print(f'{"PROJETO JÃO":.^60}')
+print('-=-'*20)
+converVOZ('BEM-VINDO MEU PATRÃO, COMO POSSO AJUDAR')
 
 
 def ouvir_microfone():
@@ -29,10 +32,11 @@ def ouvir_microfone():
 			print("Estou on...")
 			# Armazena a informacao de audio na variavel
 			audio = microfone.listen(source)
-
+			sleep(0.2)
 		try:
 			# Passa o audio para o reconhecedor de padroes do speech_recognition
 			frase = microfone.recognize_google(audio, language='pt-BR')
+
 
 		# Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
 		except sr.UnkownValueError:
@@ -48,16 +52,20 @@ while True:
 		converVOZ('Oi, chefe')
 		frase = ouvir_microfone()
 	elif frase.lower() == 'toca a boa':
+		converVOZ('É pra já')
 		pygame.init()
 		pygame.mixer.music.load('tchuco.mp3')
+		pygame.mixer.music.set_volume(0.1)
 		pygame.mixer.music.play()
 		frase = ouvir_microfone()
 		if frase.lower() == 'pare' or 'stop':
-			converVOZ('Tá maluco? Porque parou!')
-		pygame.event.wait()
+			pygame.mixer.music.stop()
+			converVOZ('POXA TAVA TÃO BOM')
+			frase = ouvir_microfone()
 		input()
-
+	elif frase.lower() == 'vou dormir' or 'vou dormi':
+		converVOZ('Bom descanso chefe!')
+		break
 	else:
-		converVOZ('Não entendi Everton-sama')
-		converVOZ('Fale novamente')
+		converVOZ('Fala pra fora que eu Não entendi')
 		frase = ouvir_microfone()
